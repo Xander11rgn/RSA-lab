210a1136673bd239083304523e7fc37cbfb70d40
Xander11rgn <xander11rgn-cool@yandex.ru>
2020-10-05 21:08:42 +0300

    RSA

---
+++ RSA/Form1.Designer.cs
@@ -0,0 +1,254 @@
+﻿namespace RSA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox7 = new System.Windows.Forms.RichTextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.richTextBox3 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.richTextBox4 = new System.Windows.Forms.RichTextBox();
+            this.richTextBox6 = new System.Windows.Forms.RichTextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.richTextBox5 = new System.Windows.Forms.RichTextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 12);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(440, 617);
+            this.richTextBox1.TabIndex = 0;
+            this.richTextBox1.Text = "";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(728, 12);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(480, 308);
+            this.richTextBox2.TabIndex = 1;
+            this.richTextBox2.Text = "";
+            // 
+            // richTextBox7
+            // 
+            this.richTextBox7.Location = new System.Drawing.Point(728, 326);
+            this.richTextBox7.Name = "richTextBox7";
+            this.richTextBox7.Size = new System.Drawing.Size(480, 303);
+            this.richTextBox7.TabIndex = 2;
+            this.richTextBox7.Text = "";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(458, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(261, 47);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Сгенерировать ключ";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // richTextBox3
+            // 
+            this.richTextBox3.Location = new System.Drawing.Point(458, 99);
+            this.richTextBox3.Name = "richTextBox3";
+            this.richTextBox3.Size = new System.Drawing.Size(261, 101);
+            this.richTextBox3.TabIndex = 4;
+            this.richTextBox3.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(522, 59);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(130, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Открытый ключ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(458, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(13, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "e";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(458, 203);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(13, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "n";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(532, 326);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(129, 20);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Закрытый ключ";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(504, 555);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(13, 13);
+            this.label5.TabIndex = 13;
+            this.label5.Text = "n";
+            // 
+            // richTextBox4
+            // 
+            this.richTextBox4.Location = new System.Drawing.Point(461, 219);
+            this.richTextBox4.Name = "richTextBox4";
+            this.richTextBox4.Size = new System.Drawing.Size(261, 101);
+            this.richTextBox4.TabIndex = 14;
+            this.richTextBox4.Text = "";
+            // 
+            // richTextBox6
+            // 
+            this.richTextBox6.Location = new System.Drawing.Point(461, 356);
+            this.richTextBox6.Name = "richTextBox6";
+            this.richTextBox6.Size = new System.Drawing.Size(261, 101);
+            this.richTextBox6.TabIndex = 16;
+            this.richTextBox6.Text = "";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(458, 340);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(13, 13);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "d";
+            // 
+            // richTextBox5
+            // 
+            this.richTextBox5.Location = new System.Drawing.Point(458, 475);
+            this.richTextBox5.Name = "richTextBox5";
+            this.richTextBox5.Size = new System.Drawing.Size(261, 101);
+            this.richTextBox5.TabIndex = 18;
+            this.richTextBox5.Text = "";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(455, 459);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(13, 13);
+            this.label6.TabIndex = 17;
+            this.label6.Text = "n";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(458, 582);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(124, 47);
+            this.button2.TabIndex = 19;
+            this.button2.Text = "Зашифровать";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(588, 582);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(131, 47);
+            this.button3.TabIndex = 20;
+            this.button3.Text = "Дешифровать";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1220, 642);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.richTextBox5);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.richTextBox6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.richTextBox4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.richTextBox7);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.richTextBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.RichTextBox richTextBox7;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.RichTextBox richTextBox3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.RichTextBox richTextBox4;
+        private System.Windows.Forms.RichTextBox richTextBox6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.RichTextBox richTextBox5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+    }
+}
+
---
+++ RSA/Form1.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Numerics;
+
+namespace RSA
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        BigInteger p, q, n, m, d, ee;
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string text = richTextBox2.Text;
+            richTextBox7.Text = "";
+            foreach (string str in text.Split(' '))
+            {
+                richTextBox7.Text += Convert.ToChar((int)BigInteger.ModPow(BigInteger.Parse(str), d, n)).ToString();
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            richTextBox2.Text = "";
+            for (int i = 0; i < richTextBox1.Text.Length; i++)
+            {
+                BigInteger text = BigInteger.ModPow(BigInteger.Parse(Convert.ToInt64(richTextBox1.Text[i]).ToString()), ee, n);
+                richTextBox2.Text += text.ToString() + " ";
+            }
+            richTextBox2.Text = richTextBox2.Text.Remove(richTextBox2.Text.Length - 1);
+        }
+
+        private bool testFerma(BigInteger n, int t)
+        {
+            for (int index = 1; index <= t; ++index)
+            {
+                if (BigInteger.ModPow(NextBigInteger(2, n - 2), n - 1, n) != 1)
+                    return false;
+            }
+            return true;
+        }
+
+        public BigInteger NextBigInteger(int bitLength)
+        {
+            Random random = new Random();
+            int a = bitLength / 8;
+            int b = bitLength % 8;
+            byte[] buffer = new byte[a + 1];
+            random.NextBytes(buffer);
+            byte c = (byte)(byte.MaxValue >> 8 - b);
+            buffer[buffer.Length - 1] &= c;
+            return new BigInteger(buffer);
+        }
+
+        public BigInteger NextBigInteger(BigInteger start, BigInteger end)
+        {
+            if (start == end)
+                return start;
+            BigInteger bigInteger1 = end;
+            BigInteger bigInteger2;
+            if (start > end)
+            {
+                end = start;
+                start = bigInteger1;
+                bigInteger2 = end - start;
+            }
+            else
+                bigInteger2 = bigInteger1 - start;
+            int num = 8 + 8 * bigInteger2.ToByteArray().Length;
+            return NextBigInteger(num + 1) * bigInteger2 / BigInteger.Pow(2, num + 1) + start;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            while (true)
+            {
+                p = NextBigInteger(1024);
+                while (!testFerma(p, 10))
+                {
+                    p = NextBigInteger(1024);
+                }
+                    
+                q = NextBigInteger(1024);
+                while (!testFerma(q, 10) || q == p)
+                {
+                    q = NextBigInteger(1024);
+
+                }
+
+                n = BigInteger.Multiply(p, q);
+                richTextBox5.Text = richTextBox4.Text = n.ToString();
+
+                m = BigInteger.Multiply(p - 1, q - 1);
+
+                ee = 3;
+                while (true)
+                {
+                    if (!(BigInteger.GreatestCommonDivisor(ee, m) == 1))
+                        ee++;
+                    else
+                        break;
+                }
+
+                BigInteger k = 2;
+                while (true)
+                {
+                    d = (k * m + 1) / ee;
+                    if (!(BigInteger.GreatestCommonDivisor(d, m) == 1))
+                        k += 2;
+                    else
+                        break;
+                }
+                richTextBox6.Text = d.ToString();
+                richTextBox3.Text = ee.ToString();
+
+                if (BigInteger.ModPow(BigInteger.ModPow(1072, ee, n), d, n) == 1072)
+                {
+                    break;
+                }
+            }
+            
+        }
+    }
+}
